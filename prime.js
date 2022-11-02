9b755c6b667ab5ca53290f649c1b0bf56f96395f
vivek singh <viveksingh2472@gmail.com>
2022-11-02 13:13:23 +0530

    all ok

--- prime.js
+++ prime.js
@@ -1,12 +1,18 @@
 //find prime
-function find_prime(n){
+function find_prime(number){
 count=0
-for(let i=0;i<=N;i++){
-    if(n%i==0){
+for(let i=0;i<=number;i++){
+    if(number%i==0){
      count++
 }
    }
-if(count==1){
+if(count==2){
 return true
 }return false
+}
+let answer=find_prime(11);
+if (answer==true){
+   console.log("Prime Number");
+}else{
+    console.log("Not a Prime Number")
 }
