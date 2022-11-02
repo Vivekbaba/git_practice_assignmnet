137b9e27556879b8433dfc3c1b47d9b46cbeb1be
gautamkumar1912 <gautamsinghgolu12@gmail.com>
2022-11-02 12:46:54 +0530

    checked

--- prime.js
+++ prime.js
@@ -1,12 +1,14 @@
 //find prime
-function find_prime(n){
-count=0
-for(let i=0;i<=N;i++){
-    if(n%i==0){
+function find_prime(N){
+let count=0
+for(let i=0;i<N;i++){
+    if(N%i==0){
      count++
 }
    }
-if(count==1){
-return true
-}return false
+if(count==2){
+return true;
+}return false;
 }
+find_prime(11);
+
